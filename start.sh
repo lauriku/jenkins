0fb6623e0c3fb7cc010bec821490896e292216fb
Lauri Kuittinen <lauriku@gmail.com>
2016-12-08 17:54:00 +0200

    fix bash condition

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[[ -ne "${PWD}/data_dir" ]] && mkdir -p ${PWD}/data_dir
+[[ ! -d "${PWD}/data_dir" ]] && mkdir -p ${PWD}/data_dir
 
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
 -v ${PWD}/data_dir:/var/jenkins_home -u ${user_id}:${group_id} -p 8080:8080 lauriku/jenkins
